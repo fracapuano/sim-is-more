60a29cea48e8983b1aa68c74ecab582063405db4
fracapuano <francesco_capuano@aol.com>
2023-12-04 23:13:52 +0100

    remove: useless file

--- prova.py
+++
@@ -1,7 +0,0 @@
-import json
-from src.interfaces.hwnats_api import HWNASBenchAPI
-
-api = HWNASBenchAPI(file_path_or_dict="HW-NAS-Bench-v1_0.pickle", search_space="fbnet")
-
-with open("fbnet_cached_metrics.json", "w") as f:
-    json.dump(api.HW_metrics["fbnet"], f)
