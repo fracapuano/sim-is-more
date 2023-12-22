c77cd1c27dc8201471dc68865700fe6b2588d4b3
fracapuano <francesco_capuano@aol.com>
2023-12-22 19:56:39 +0100

    add: MarcellaPlusEnv, implementing synthetic devices in reset

--- custom_env/__init__.py
+++ custom_env/__init__.py
@@ -1,11 +1,13 @@
 from .nas_env import *
 from .oscar import *
 from .marcella import *
+from .marcella_plus import *
 from .utils import *
 
 envs_dict = {
     "nasenv": NASEnv,
     "oscar": OscarEnv,
-    "marcella": MarcellaEnv
+    "marcella": MarcellaEnv,
+    "marcella-plus": MarcellaPlusEnv
 }
 
