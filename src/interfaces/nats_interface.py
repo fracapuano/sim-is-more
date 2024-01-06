9c4a5e2a3ccaca681b61d578f38425b6ff3fcdbf
fracapuano <francesco_capuano@aol.com>
2024-01-06 16:04:18 +0100

    remove: useless read operation

--- src/interfaces/nats_interface.py
+++ src/interfaces/nats_interface.py
@@ -19,10 +19,6 @@ class NATS_Interface(Base_Interface):
     NATS-specific search space interface.
     Also implements hardware-awareness through the use of a lookup table.
     """
-    path_to_synthetic_devices = str(get_project_root()) + "/searchspaces/nats_interface_syntheticdevices_lookuptable.json"
-    # reading the synthetic-devices lookup table
-    with open(path_to_synthetic_devices, "r") as lookup_file:
-        synthetic_lookup_table = {int(k): v for k, v in json.load(lookup_file).items()}
 
     def __init__(self, 
                  datapath:str=str(get_project_root()) + "/searchspaces/nats_blocks.json",
@@ -30,8 +26,7 @@ class NATS_Interface(Base_Interface):
                  use_lookup_table:bool=True,
                  path_to_lookup:Optional[str]=str(get_project_root()) + "/searchspaces/nats_interface_lookuptable.json", 
                  path_to_lookup_index:Optional[str]=str(get_project_root()) + "/searchspaces/nats_arch_index.json",
-                 target_device:Optional[Text]=None,
-                 use_synthetic_devices:bool=False):
+                 target_device:Optional[Text]=None):
         
         # parent init, loading the datapath
         super().__init__(datapath)
@@ -46,12 +41,8 @@ class NATS_Interface(Base_Interface):
             )
         elif path_to_lookup is not None:
             # either loading the lookup table or the synthetic devices lookup table
-            if not use_synthetic_devices:
-                with open(path_to_lookup, "r") as lookup_file:
+            with open(path_to_lookup, "r") as lookup_file:
                     self.lookup_table = {int(k): v for k, v in json.load(lookup_file).items()}
-            else:
-                # using a synthetic lookup table stored in memory once only
-                self.lookup_table = self.synthetic_lookup_table
 
             with open(path_to_lookup_index, "r") as lookup_index_file:
                 self.architecture_to_index = json.load(lookup_index_file)
