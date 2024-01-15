c5f81a0fe446d6dda32fe06aaa6da90846ccca94
fracapuano <francesco_capuano@aol.com>
2024-01-15 18:14:46 +0100

    fix: reading args from config file

--- test.py
+++ test.py
@@ -1,18 +1,13 @@
 import re
-import time
+import json
 import pickle
 import argparse
 import numpy as np
-from src import (
-    seed_all, 
-    NATS_Interface,
-)
-from policy import (
-    Policy, 
-    TransitionsHistoryWrapper
-)
 from custom_env import envs_dict
 from typing import Text, Optional
+from train import create_searchspace
+from src.utils import to_scientific_notation
+from policy import Policy, TransitionsHistoryWrapper
 
 
 def boolean_string(s):
@@ -37,15 +32,16 @@ def parse_args()->object:
         (object): args parser
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model-path", default="models/", type=str, help="Path to which the model to incrementally train is stored")
-    parser.add_argument("--dataset", default="cifar10", type=str, help="Dataset on which to run the search. One in ['cifar10', 'cifar100', 'imagenet']")
-    parser.add_argument("--target-device", default="raspi4", type=str, help="Target device. For Hardware aware search only.")
-    parser.add_argument("--verbose", default=0, type=int, help="Verbosity value")
+    parser.add_argument("--folder-path", default="models/", type=str, help="Path at which to find the model and the training config are stored")
+    parser.add_argument("--use-custom-device", action="store_true", help="When not provided, uses the custom device provided in the training config")
+    parser.add_argument("--target-device", default="edgegpu", type=str, help="Target device for hardware aware search")
+    
+    parser.add_argument("--verbose", default=1, type=int, help="Verbosity level")
     parser.add_argument("--test-episodes", default=5, type=int, help="Number of test episodes for the RL agent")
     parser.add_argument("--seed", default=777, type=int, help="Random seed setted")
-    parser.add_argument("--best-ever", action="store_true", help="When provided, policy is prompted to return fittest individual EVER.")
+    parser.add_argument("--best-ever", action="store_true", help="When provided, policy is prompted to return fittest individual across all test episodes.")
     parser.add_argument("--render", action="store_true", help="When provided, triggers rendering.")
-    parser.add_argument("--save-trajectories", action="store_true", help="When provided, saves all tested trajectories (initial_net, final_net).")
+    parser.add_argument("--save-trajectories", action="store_true", help="When provided, saves all tested trajectories. Starting and terminal states only")
 
     #parser.add_argument("--default", action="store_true", help="Default mode, ignore all configurations")
     parser.add_argument("--debug", action="store_true", help="Default mode, ignore all configurations")
@@ -56,95 +52,77 @@ def main():
     """Performs training and logs info to wandb."""
     args = parse_args()
 
-    model_path=args.model_path
-    dataset=args.dataset
-    target_device=args.target_device
-    verbose=args.verbose
-    test_episodes=args.test_episodes
-    seed=args.seed
-    best_ever=args.best_ever
-    save_trajectories=args.save_trajectories
-    
-    if args.debug: 
-        model_path="models/oscar/edgegpu/lr=3.0e-4/gamma=0.6/seed=777/PPO_oscar_1.0e5/PPO_oscar_1.0e5.zip"
-        dataset="ImageNet16-120"
-        target_device="edgegpu"
-        test_episodes=1
-        seed=0
-        verbose=1
-        best_ever=False
+    with open(f"{args.folder_path}/training_config.json", "r") as f:
+        configuration = json.load(f)
+    # reading setting parameters from training config file
+    algorithm, env_name = configuration["algorithm"], configuration["env_name"]
 
-    # set seed for reproducibility
-    seed_all(seed=seed)
-    
-    # model path is always in the format directory/<Algorithm>_<Env>_<TrainTimesteps>.zip
-    algorithm, environment, *_ = model_path.split("/")[-1].split("_")
-
-    # accessing to the env defined at the policy level
-    searchspace_interface = NATS_Interface(dataset=dataset)
-
+    searchspace_interface = create_searchspace(searchspace=configuration["searchspace"], dataset=configuration["dataset"])
     # create env (gym.Env)
-    env = envs_dict[environment.lower()](searchspace_api=searchspace_interface)
+    env = envs_dict[env_name.lower()](
+        searchspace_api=searchspace_interface,
+        scores=configuration["score_list"],
+        target_device=args.target_device if args.use_custom_device else configuration["target_device"],
+        weights=[configuration["task_weigth"], configuration["hardware_weigth"]] 
+        )
     
-    # setting a target device for hardware aware search at test time
-    env.target_device = target_device
+    if env_name == "marcella-plus":
+        env = TransitionsHistoryWrapper(env=env, history_len=configuration["history_len"])
 
-    if environment == "marcella-plus":
-        env = TransitionsHistoryWrapper(env=env, history_len=extract_historylen_from_path(model_path))
+    model_name = \
+        f"{args.folder_path}/{algorithm.upper()}_{env_name}_{to_scientific_notation(configuration['train_timesteps'])}.zip"
 
     # instantiate a testing suite
     policy = Policy(
         algo=algorithm,
         env=env,
-        load_from_pathname=model_path
+        load_from_pathname=model_name
     )
 
-    if verbose > 0: 
-        print(f"Loading model from {model_path}")
-        print(f"Testing {algorithm} on {environment}")
+    if args.verbose > 0: 
+        print(f"Loading model from {model_name}")
+        print(f"Testing {algorithm} on {env_name}")
     
-    # saving up the episode return and time duration
-    returns, durations = np.zeros(test_episodes), np.zeros(test_episodes)
-    
+    # saving up the episode return
+    returns = np.zeros(args.test_episodes)
     initial_nets, episode_bests = [], []
 
-    for ep in range(test_episodes):
+    for ep in range(args.test_episodes):
         done = False
         obs, info = env.reset() # Reset environment to initial state
         # save initial network
         initial_net = env.current_net.architecture
         initial_nets.append(initial_net)
-
+        
+        # accumulating the episode return
         episode_return = 0
-        start = time.time()
-
         while not done:  # Until the episode is over
             action = policy.predict(observation=obs, deterministic=True)  # deterministic is True while evaluating            
             obs, reward, terminated, truncated, info = env.step(action)	# Step the simulator to the next timestep
-
             done = terminated or truncated
-
             episode_return += reward
+
             if args.render:
                 env.render()
-                
-        durations[ep] = time.time() - start
+
         returns[ep] = episode_return
-    
+        # saving the best individual of the episode
         best_individual = env.current_net.architecture
         episode_bests.append(best_individual)
-        print(f"Network Designed: {best_individual}")
+        if args.verbose:
+            print(f"Network Designed: {best_individual}")
     
     print("Average episode return {:.4g}".format(returns.mean()))
-    if best_ever:
+    if args.best_ever:
         best_ever_net = max(episode_bests, key=lambda x: searchspace_interface.list_to_accuracy(x))
-        print(f"Out of {test_episodes} test episodes, the best network designed is {best_ever_net}")
+        print(f"Out of {args.test_episodes} test episodes, the best network designed is {best_ever_net}")
         print("With a validation accuracy of {:.5g}".format(searchspace_interface.architecture_to_accuracy(best_individual)))
 
-    if save_trajectories:
+    if args.save_trajectories:
         # saving all final networks obtained
-        with open("test_results.pkl", "wb") as results:
+        with open(f"{args.folder_path}/test_results.pkl", "wb") as results:
             pickle.dump(list(zip(initial_nets, episode_bests)), results)
+
 
 if __name__=="__main__":
     main()
